438b544f4a24ff914978eb9e0bac819e9696ceff
javaclerk <chg5638@gmail.com>
2023-02-24 16:19:00 +0900

    asdf

---
+++ main_program/led/func/ledpattern.cc
@@ -0,0 +1,17 @@
+#include "ledpattern.h"
+
+using namespace std;
+
+ledpattern::ledpattern(){
+    
+}
+
+void ledpattern1(){
+
+    switch((LED_TYPE)(gpio_num -1))
+    {
+        case LED_TYPE_GNSS:
+        
+
+    }
+}
--- main_program/led/includes/ledpattern.h
+++ main_program/led/includes/ledpattern.h
@@ -1,9 +1,17 @@
+#include <iostream>
+#include <fstream>
+
 #include "ledfunc.h"
 
 using namespace std;
 
-class ledapttern{
+class ledpattern{
     public:
 
-    
+    ledpattern();
+
+    void ledpattern1();
+    void ledpattern2();
+    void ledpattern3();
+
 }
